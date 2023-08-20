33bae05f7cdd0e0aed336dcecba5763a28cd305a
KolBa228 <technotrashinfo@gmail.com>
2023-08-20 12:00:49 +0100

    asdasdasd

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,62 +1,113 @@
-import { useEffect, useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
+// import { Link, useNavigate, useLocation } from 'react-router-dom';
+// import APIservices from "services/fetch";
+
+// const Movies = () => {
+//   const [searchKeyword, setSearchKeyword] = useState('');
+//   const [searchResults, setSearchResults] = useState([]);
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//     const handleSearch = async () => {
+//     try {
+//       navigate(`/movies?movieKeyWord=${searchKeyword}`);
+//       setSearchResults([]);
+
+//       const response = await APIservices.fetchMoviesByName(searchKeyword);
+//       setSearchResults(response.results);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//     };
+  
+  
+//   const handleInputChange = (event) => {
+//     setSearchKeyword(event.target.value);
+//   };
+
+//   useEffect(() => {
+//     const params = new URLSearchParams(location.search);
+//     const movieKeyWordParam = params.get('movieKeyWord');
+
+//     if (movieKeyWordParam) {
+//       setSearchKeyword(movieKeyWordParam);
+//       handleSearch();
+//     }
+//   }, [location.search]);
+
+//   useEffect(() => {
+//     handleSearch();
+//   }, [searchKeyword]);
+
+//   return (
+//     <div>
+//       <h1>Movies By Search</h1>
+//       <input
+//         type="text"
+//         value={searchKeyword}
+//         onChange={handleInputChange}
+//         placeholder="Search movies..."
+//       />
+//       <button onClick={handleSearch}>Search</button>
+//       <ul>
+//         {searchResults.map(movie => (
+//           <li key={movie.id}>
+//             <Link to={`/movies/${movie.id}`}>
+//               <img src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+//               <p>{movie.title}</p>
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Movies;
+
+import { useState } from "react";
+import { useSearchParams, Link } from "react-router-dom";
 import APIservices from "services/fetch";
 
 const Movies = () => {
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const navigate = useNavigate();
-  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const handleSearch = async () => {
+  const handleSearch = async () => {
+    searchParams.set("movieKeyWord", searchKeyword);
+    setSearchParams(searchParams);
+    
     try {
-      navigate(`/movies?movieKeyWord=${searchKeyword}`);
-      setSearchResults([]);
-
       const response = await APIservices.fetchMoviesByName(searchKeyword);
       setSearchResults(response.results);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("ASHIBKA", error);
     }
-    };
-  
-  
+  };
+
   const handleInputChange = (event) => {
     setSearchKeyword(event.target.value);
   };
 
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const movieKeyWordParam = params.get('movieKeyWord');
-
-    if (movieKeyWordParam) {
-      setSearchKeyword(movieKeyWordParam);
-      handleSearch();
-    }
-  }, [location.search]);
-
-  useEffect(() => {
-    handleSearch();
-  }, [searchKeyword]);
-
-
-
-
   return (
     <div>
-      <h1>Movies By Search</h1>
+      <h1>Search</h1>
       <input
         type="text"
         value={searchKeyword}
         onChange={handleInputChange}
-        placeholder="Search movies..."
+        placeholder="Find movie..."
       />
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSearch}>Find</button>
       <ul>
-        {searchResults.map(movie => (
+        {searchResults.map((movie) => (
           <li key={movie.id}>
             <Link to={`/movies/${movie.id}`}>
-              <img src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+              <img
+                src={`http://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                alt={movie.title}
+              />
               <p>{movie.title}</p>
             </Link>
           </li>
@@ -67,3 +118,4 @@ const Movies = () => {
 };
 
 export default Movies;
+
